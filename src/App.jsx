da641f1ab608023bbab0697386fa1d38ce99d4dc
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-01-18 19:21:07 +0530

    Installed & setup files

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+     <header>
+       <h1 className='text-3xl text-red-400'>Global News</h1>
+     </header>
+  )
+}
+
+export default App;
