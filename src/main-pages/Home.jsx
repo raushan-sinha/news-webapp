9f7a54aac5cacb7467292bb7523f34baaa2e59d5
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-01-21 22:49:57 +0530

    Removed code from main & added padding

--- src/main-pages/Home.jsx
+++ src/main-pages/Home.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <main className='min-h-screen bg-[#101923] text-white'>
-            <h1 className='text-center p-40'>Home are coming soon</h1>
+        <main className='p-40'>
+            <h1 className='text-center'>Home is coming soon</h1>
         </main>
     )
 }
