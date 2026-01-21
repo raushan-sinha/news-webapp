91a8e9025607087fa934a70847df8ae1891a1180
Raushan Sinha <raushankrsinha2004@gmail.com>
2026-01-21 20:41:01 +0530

    Code updated with - main and h1 syles

--- src/main-pages/Home.jsx
+++ src/main-pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 
 const Home = () => {
-  return (
-    <h1>Home are coming soon</h1>
-  )
+    return (
+        <main className='min-h-screen bg-[#101923] text-white'>
+            <h1 className='text-center p-40'>Home are coming soon</h1>
+        </main>
+    )
 }
 
 export default Home;
